69fb413306d0a9ec1483beac67edc2b34ed664e1
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-31 13:50:08 -0700

    fix errors in linking stylesheets
    Co-authored-by: Patrick Dolan <dolanp1992@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#shoutForm").submit(function(event) {
     const shout = $("input#shout1").val();
     $(".userShout").append(shout.toUpperCase());
-    $("shoutDisplay").show();
+    $("#shoutDisplay").show();
     event.preventDefault();
   });
 });
