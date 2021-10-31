4ef8ccddfa05aff4eb1af61e70ca1a1d0d77d033
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-31 15:58:32 -0700

    added click event for button-2. button-2 displays the wrong thing on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-const shoutArray= new Array();
+const shoutArray= [];
   $("#shoutForm").submit(function(event) {
     const shout = $("input#shout1").val();
-    
+    shoutArray.push(shout);
     $("#shoutDisplay").show();
     $("#shoutForm").trigger("reset");
     $("#shoutDisplay").fadeOut(7500);
 
     event.preventDefault();
   });
-  document.getElementById("button-1").onclick(function(){
-
+  document.getElementById("button-2").addEventListener("click", function(){
+    const length = shoutArray.length - 1;
+    const randNum = 1+ Math.random() * length;
+    $(".retrieved").prepend(shoutArray[randNum]);
   });
 });
