e062295086ef0c15de0c77780e6c7050ecd86c94
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-31 12:26:19 -0700

    add scripts
    Co-authored-by: Patrick Dolan <dolanp1992@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#shoutForm").submit(function(event) {
+    const shout = $("input#shout1").val();
+    $(".userShout").append(shout.toUpperCase());
+    $("shoutDisplay").show();
+    event.preventDefault();
+  });
+});
